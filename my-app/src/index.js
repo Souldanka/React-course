80aa36c8d8efec66f1d5573608a13a1b1261f043
Danylo <danillazorenko12@gmail.com>
2021-03-13 18:36:56 +0200

    add todo app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
+
+// const el = (
+//   <div>
+//     <span>One</span> 
+//     {/* Отдельно нельзя создать 2 span, нужно обернуть их в div */}
+//     <span>Two</span>
+//   </div>
+// )
 // JSX
 // const el = <h1> Hello</h1>
 
+//  {} можно использовать для передачи значения атрибутов
 // React компонент
 const TodoList = () => {
+  const items = ["Drink Coffe", "Build Awesome App"];
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Build Awesome App</li>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
     </ul>
   )
 };
@@ -21,13 +31,38 @@ const AppHeader = () => {
 
 // React компонент
 const SearchPanel = () => {
-  return  <input placeholder="search" />
+  // {} можно использовать для передачи значения атрибутов
+  const searchText = "Type here to search";
+  // Передать стиль 
+  const searchStyle = {
+    fontSize: "20px"
+  };
+  return  <input 
+    style = {searchStyle}
+    placeholder={searchText} />
+    // class в html
+    // className в JSX
+    // for в html
+    // htmlFor = " " в JSX
+
+ 
 };
 
 // React компонент
 const App = () => {
+
+  
+  //  html по умолчанию эскепится 
+  // const isLoggedIn = true;
+  // const loginBox = <span>Log in please</span>;
+  // const welcomeBox = <span>Welcome Back</span>
+
+  // const loginBox = null 
+  // не будет ошибка, просто не отобразиться на странице
+
   return (
     <div>
+      {/* { isLoggedIn ? welcomeBox : loginBox } */}
       <AppHeader/>
       <SearchPanel />
       <TodoList />
